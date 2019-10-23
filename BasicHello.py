0c14b3a22c9ef68b0ac9c004146adf283b7821d1
ParitoshChaudhary <paritoshc.at@gmail.com>
2019-10-23 17:09:44 +0530

    Dynamic routing dated 23.10.2019

--- BasicHello.py
+++ BasicHello.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #127.0.0.1:5000/
 def index():
     return '<h1> THIS IS THE FIRST PAGE FROM FLASK<h1>'
+
+@app.route('/info') #127.0.0.1:5000/info
+def info():
+    return "<h1> THIS IS THE SAMPLE INFORMATION<h1>"
 
 if __name__ == '__main__':
     app.run()
---
+++ DynamicRouting.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/name/<name>")
+def name(name):
+    return f"<h1> THIS IS A MESSAGE TO WELCOME {name}<h1>"
+
+@app.route("/name_up/<name>")
+def nameUpper(name):
+    return f"<h1> THIS IS A MESSAGE FOR UPPER {name.upper()}<h1>"
+
+if __name__ == "__main__":
+    app.run()
