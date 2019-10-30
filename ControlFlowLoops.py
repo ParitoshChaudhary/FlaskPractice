3b936946d2ca43608395424f2a7aadfcaacbbbb2
ParitoshChaudhary <paritoshc.at@gmail.com>
2019-10-30 16:17:10 +0530

    Controlflow, Inheritence and url dated 30.10.2019 .

---
+++ ControlFlowLoops.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    mylist = [1,2,3,4,5]
+    return render_template('ControlFlowForLoop.html', mylist = mylist)
+
+if __name__ == '__main__':
+    app.run(debug=True)
